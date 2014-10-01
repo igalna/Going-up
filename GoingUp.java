dd5d1696b8068838c7d18cdf22c5141ba2716605
igalna <igalna@gmail.com>
2014-10-01 16:28:06 +0100

    Second commit

--- GoingUp.java
+++ GoingUp.java
@@ -2,10 +2,36 @@ import java.util.Scanner;
 
 public class GoingUp {
     public static void main(String[] args) {
+        
         Scanner keyboard = new Scanner(System.in);
         
-        System.out.print("Enter a series of numbers > ");
-        System.out.print("More Printing");
+        boolean total = true;
+        boolean current = true;
         
+        int userNum1;
+        int userNum2;
+        
+        System.out.print("Enter a number > ");
+        userNum1 = keyboard.nextInt();
+        
+        while (userNum1 != -1) {
+            System.out.print("Enter another number > ");
+            userNum2 = keyboard.nextInt();
+            
+            if (userNum2 != (userNum1+1)) {
+                total = false;
+            }
+            else {
+                total = true;
+            }
+            userNum1 = userNum2;
+        }
+        
+        if (total) {
+            System.out.println("YES!");
+        }
+        else if (!total) {
+            System.out.println("NO!");
+        }
     }
 }
