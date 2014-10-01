7b2bd20b36a076877db2ed6514dca400a59000b2
unknown <ijohns03@CSPC115.dcs.bbk.ac.uk>
2014-10-01 15:49:06 +0100

    Second Commit

---
+++ GoingUp.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class GoingUp {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.print("Enter a series of numbers > ");
+        System.out.print("More Printing");
+        
+    }
+}
